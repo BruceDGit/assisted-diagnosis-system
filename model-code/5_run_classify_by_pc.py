ea7ad5465262f2ab8afff1252e381760782c078d
BruceDGit <wayne_s@126.com>
2020-08-06 09:19:04 +0800

    Add entry function of classify

---
+++ model-code/5_run_classify_by_pc.py
@@ -0,0 +1,25 @@
+"""
+    执行模型的训练与测试
+        采用包含95%累计贡献率的特征数据
+"""
+from models.Models import *
+
+CLASS_1_FILE_PATH = './data/new_class_1.dat'
+CLASS_2_FILE_PATH = './data/new_class_2.dat'
+CLASS_3_FILE_PATH = './data/new_class_3.dat'
+CLASS_4_FILE_PATH = './data/new_class_4.dat'
+
+
+if __name__ == '__main__':
+    print("class_1:")
+    class_1(CLASS_1_FILE_PATH)
+    print("\nclass_2:")
+    class_2(CLASS_2_FILE_PATH)
+    print("\nclass_3:")
+    class_3(CLASS_3_FILE_PATH)
+    print("\nclass_4:")
+    class_4(CLASS_4_FILE_PATH)
+    pass
+
+
+
